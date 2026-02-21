9ed4f1bfe6c25b30afa69d06ba357579ba1880c6
IDGS-804-23001922 <oscarloredo588@gmail.com>
2026-02-21 09:48:05 -0600

    Migraciones 21/02/26

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from flask_wtf.csrf import CSRFProtect
 from config import DevelopmentConfig
 from flask import g
 import forms 
+from flask_migrate import Migrate
 
 from models import db
 from models import Alumnos
 
 app = Flask(__name__)
 app.config.from_object(DevelopmentConfig)
+db.init_app(app)
+migrate = Migrate(app,db)
 csrf=CSRFProtect()
 
 @app.route("/",methods=['GET','POST'])
@@ -28,8 +31,10 @@ def alumnos():
     alumno_class=forms.UserForm(request.form)
     if request.method=='POST':
         alum=Alumnos(nombre=alumno_class.nombre.data,
-               apaterno=alumno_class.apaterno.data,
-               email=alumno_class.email.data)
+               apellidos=alumno_class.apellidos.data,
+               email=alumno_class.email.data,
+               telefono=alumno_class.telefono.data)
+        
         db.session.add(alum)
         db.session.commit()
         return redirect(url_for('index'))
@@ -42,17 +47,63 @@ def detalles():
         id=request.args.get('id')
         alumn1=db.session.query(Alumnos).filter(Alumnos.id==id).first()
         nombre=alumn1.nombre
-        apaterno=alumn1.apaterno
+        apellidos=alumn1.apellidos
         email=alumn1.email
-    return render_template("detalles.html", nombre=nombre, apaterno=apaterno, email=email)
+        telefono=alumn1.telefono
+    return render_template("detalles.html", nombre=nombre, apellidos=apellidos, email=email, telefono=telefono)
 
-@app.route('/editar', methods=['GET', 'POST'])
+
+@app.route('/modificar', methods=['GET', 'POST'])
 def modificar():
-      return render_template("modificar.html")
+    create_form = forms.UserForm(request.form)
+
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alumn1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = alumn1.id
+        create_form.nombre.data = alumn1.nombre
+        create_form.apellidos.data = alumn1.apellidos
+        create_form.email.data = alumn1.email
+        create_form.telefono.data = alumn1.telefono
+
+    if request.method == 'POST':
+        id = create_form.id.data
+        alumn1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        alumn1.nombre = str.rstrip(create_form.nombre.data)
+        alumn1.apellidos = create_form.apellidos.data
+        alumn1.email = create_form.email.data
+        alumn1.telefono = create_form.telefono.data
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template("modificar.html", form=create_form)
+
+@app.route('/eliminar', methods=['GET', 'POST'])
+def eliminar():
+    create_form = forms.UserForm(request.form)
+
+    if request.method == 'GET':
+        id = request.args.get('id')
+        alumn1 = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        create_form.id.data = alumn1.id
+        create_form.nombre.data = alumn1.nombre
+        create_form.apellidos.data = alumn1.apellidos
+        create_form.email.data = alumn1.email
+        create_form.telefono.data = alumn1.telefono
+
+    if request.method == 'POST':
+        id = create_form.id.data
+        alumn = db.session.query(Alumnos).filter(Alumnos.id == id).first()
+        db.session.delete(alumn)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template("eliminar.html", form=create_form)
+
+
 
 if __name__ == '__main__':
 	csrf.init_app(app)
-	db.init_app(app)
 
 	with app.app_context():
 		db.create_all()
---
+++ bdidgs804.sql
@@ -0,0 +1,78 @@
+-- MySQL dump 10.13  Distrib 8.0.30, for macos12 (x86_64)
+--
+-- Host: localhost    Database: bdidgs804
+-- ------------------------------------------------------
+-- Server version	8.0.30
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `alembic_version`
+--
+
+DROP TABLE IF EXISTS `alembic_version`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alembic_version`
+--
+
+LOCK TABLES `alembic_version` WRITE;
+/*!40000 ALTER TABLE `alembic_version` DISABLE KEYS */;
+INSERT INTO `alembic_version` VALUES ('bce8ac91b4d7');
+/*!40000 ALTER TABLE `alembic_version` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `alumnos`
+--
+
+DROP TABLE IF EXISTS `alumnos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `alumnos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(50) DEFAULT NULL,
+  `email` varchar(120) DEFAULT NULL,
+  `create_date` datetime DEFAULT NULL,
+  `apellidos` varchar(50) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `alumnos`
+--
+
+LOCK TABLES `alumnos` WRITE;
+/*!40000 ALTER TABLE `alumnos` DISABLE KEYS */;
+INSERT INTO `alumnos` VALUES (9,'carlos','carlos@gmail.com','2026-02-14 09:26:00','as','1234567890'),(10,'Daniel','chavez@gmail.com','2026-02-14 09:26:00','Manriquez','1234567890'),(11,'Oscar','oscarcaceres069@gmail.com','2026-02-20 17:45:32','Caceres','1234567890');
+/*!40000 ALTER TABLE `alumnos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-02-21  9:45:07
